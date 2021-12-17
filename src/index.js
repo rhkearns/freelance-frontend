fef42e41762f75c87830c00178ad050ac1f665cc
Cole-Fasano <cole.fasano@outlook.com>
2021-12-17 10:17:01 -0600

    write global partial and touch landing style page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './sassStyles/_global.scss'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
