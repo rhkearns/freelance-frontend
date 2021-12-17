077b843272d687d970ff7480f507ffab9820110b
rhkearns <ryankearns217@gmail.com>
2021-12-17 11:21:51 -0500

    build out create project page

--- src/pages/CreateProject/CreateProject.jsx
+++ src/pages/CreateProject/CreateProject.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const CreateProject = () => {
+// Components
+import ProjectForm from './ProjectForm'
+// Services
+import { createProjcet } from '../../services/projectService';
+
+const CreateProject = (props) => {
+  const navigate = useNavigate()
+  const [project, setProject] = useState('')
+  
+  const formData = {
+    project: project,
+    owner: props.user.profile,
+  }
+
+  const handleCreateProject = async (e) => {
+    e.preventDefault()
+    try {
+      const newProject = await CreateProject(formData)
+      console.log('new project data', newProject);
+      navigate('/projects')
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
-    <h1>Create Project</h1>
+    <>
+      <h1>Create Project</h1>
+      <ProjectForm 
+        project={project}
+        setProject={setProject}
+        handleCreateProject={handleCreateProject}
+      />
+    </>
   )
 }
 
