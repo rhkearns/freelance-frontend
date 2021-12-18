951c566f52906bbf6f63fd26f96f3f3813f0eca2
Cole-Fasano <cole.fasano@outlook.com>
2021-12-18 12:35:20 -0600

    write rough jsx structure

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ProjectList from '../ProjectList';
+import './profile.scss'
 
 const Profile = ({user}) => {
   
   
   return (
     <>
-      <h1>Profile</h1>
-      <ProjectList user={user}/>
-      <h1>Client List</h1>
+      <div className="profile-card">
+        <h3>{user.name}</h3>
+      </div>
+      <div className="project-list">
+        <ProjectList user={user}/>
+      </div>
+      <div className="clients">
+        <h1>Client List</h1>
+      </div>
     </>
   )
 }
