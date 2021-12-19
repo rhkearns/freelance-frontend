6b120c437e4147942f424a19f7e09c57e6ace54e
rhkearns <ryankearns217@gmail.com>
2021-12-19 15:02:26 -0500

    add client form addeffects

--- src/pages/AddClient/ClientForm.jsx
+++ src/pages/AddClient/ClientForm.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const ClientForm = () => {
+const ClientForm = ({handleCreateClient, handleChange, validForm, setValidForm, formData, setFormData}) => {
+  const formElement = useRef()
+
+  useEffect(() => {
+    formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+  }, [formData])
+  
   return (
     <h1>Client Form</h1>
   )
