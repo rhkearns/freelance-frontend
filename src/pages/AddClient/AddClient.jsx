cd2dddaab0d93c4acca8c92c6b83e7a229ac3d49
rhkearns <ryankearns217@gmail.com>
2021-12-19 15:00:04 -0500

    build out addclient.jsx"

--- src/pages/AddClient/AddClient.jsx
+++ src/pages/AddClient/AddClient.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
-const AddClient = () => {
+import ClientForm from './ClientForm'
+import { addClient } from '../../services/clientService';
+
+const AddClient = (props) => {
+  const navigate = useNavigate()
+  const [client, setClient] = useState()
+  const {formData, setFormData} = useState({
+    name: '',
+    email: ''
+  })
+  const [validForm, setValidForm] = useState(false)
+  
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value})
+  }
+
+  const handleCreateClient = async (e) => {
+    e.preventDefault()
+    try {
+      const newClient = await addClient(formData)
+      props.setClients([...props.clients, newClient])
+      navigate('/clients')
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
-    <h1>Add Client</h1>
+    <>
+      <h1>Add Client</h1>
+      <ClientForm 
+        client={client}
+        setClient={setClient}
+        handleCreateClient={handleCreateClient}
+        handleChange={handleChange}
+        validForm={validForm}
+        setValidForm={setValidForm}
+        formData={formData}
+        setFormData={setFormData}
+      />
+    </>
   )
 }
 
