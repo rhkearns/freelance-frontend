3556f2f8d613e5482989552fcd143b8ffb3bd1ff
Cole-Fasano <cole.fasano@outlook.com>
2021-12-20 13:52:25 -0600

    prep for merge

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -9,20 +9,19 @@ const Profile = ({user, projects}) => {
     <div className="prof-page">
       <div className="prof-side-bar">
         <div className="prof-picture">
-          <div className="circle"></div>
         </div>
         <div className="prof-card">
           <h1>{user.name}</h1>
         </div>
         <div className="side-nav">
           <ul>
-            <br/><br/><br/><br/>
+            <br/><br/><br/>
             <li>Client List</li>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/>
             <li>Create New Project</li>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/>
             <li>Another Link</li>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/>
             <li>Log Out</li>
           </ul>
         </div>
@@ -49,3 +48,13 @@ export default Profile
 //   const selectedClient = clients.filter((state) => state.id === id)
 //   await setCurrentClient({...selectedClient[0]})
 // }
+
+
+/* <div className="project">
+{projects.map((p)=> (
+  <ProjectCard
+    title={p.title}
+    startDate={p.startDate}
+    endDate={p.endDate}
+  />
+))} */
