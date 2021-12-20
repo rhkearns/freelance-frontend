3700094430afd7fb5709f11fef0902bc8c039846
rhkearns <ryankearns217@gmail.com>
2021-12-20 13:39:21 -0500

    format date

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment'
 import './project-card.scss'
 
 const ProjectCard = (props) => {
@@ -7,8 +8,8 @@ const ProjectCard = (props) => {
       <h1>{props.project.title}</h1>
       <h3>{props.project.clientList}</h3>
       <h3>Status: {props.project.is_Active}</h3>
-      <h3>Start Date: {props.project.startDate}</h3>
-      <h3>End Date: {props.project.endDate}</h3>
+      <h3>Start Date: {moment(props.project.startDate).format('MM/DD/YYYY')}</h3>
+      <h3>End Date: {moment(props.project.endDate).format('MM/DD/YYYY')}</h3>
       <h3>Hourly Rate: ${props.project.hourlyRate}</h3>
     </div>
 
