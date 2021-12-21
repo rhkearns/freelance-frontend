5963e411897aab8b325f50b5dced0481a57dc8a5
Cole-Fasano <cole.fasano@outlook.com>
2021-12-20 20:56:44 -0600

    comment out nav for organization

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,20 +7,20 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <nav>
           <ul className="user-nav">
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/projects">Placeholder for logo</Link></li>
+            {/* <li>Welcome, {user.name}</li> */}
+            <li><Link to="/projects"> Eventual Home Link </Link></li>
             <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
+            {/* <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/clients">Clients</Link></li>
             <li><Link to="/newProject">Add A Project</Link></li>
-            <li><Link to="/newClient">Add A Client</Link></li>
+            <li><Link to="/newClient">Add A Client</Link></li> */}
             <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
           </ul>
         </nav>
       :
         <nav>
           <ul className="reg-nav">
-            <li><Link to="/projects">Placeholder for logo</Link></li>
+            <li><Link to="/projects"> Eventual Home Link </Link></li>
             <li><Link to="/login">Log In</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
           </ul>
