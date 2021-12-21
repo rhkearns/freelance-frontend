99ea5f7879c77e26d0e8ab7942a1d12caa85cd6d
rhkearns <ryankearns217@gmail.com>
2021-12-21 11:57:42 -0500

    build out invoice card

---
+++ src/components/Invoice/InvoiceCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import moment from 'moment'
+
+const InvoiceCard = (props) => {
+  return (
+    <>
+      <h1>{props.invoice.title}</h1>
+      <h3>Client: {props.invoice.client.name}</h3>
+      <h3>Date Created: {moment(props.invoice.dateCreated).format('MM/DD/YYYY')}</h3>
+      {props.invoice.dueDate && 
+        <h3>Due Date: {moment(props.invoice.dueDate).format('MM/DD/YYYY')}</h3>
+      }
+      <h3>Hourly Rate: {props.invoice.hourlyRate}</h3>
+      <h3>Hours Worked: {props.invoice.hoursWorked}</h3>
+    </>
+  )
+}
+
+export default InvoiceCard
