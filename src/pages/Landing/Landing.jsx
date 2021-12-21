33477e017e1c72c90398168209dece02310075aa
Cole-Fasano <cole.fasano@outlook.com>
2021-12-21 12:58:20 -0600

    write conditional nav and fix white space

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,33 +1,35 @@
 import { Link } from 'react-router-dom'
 
-import styles from './Landing.module.css'
 import './landing.scss'
 
 const Landing = ({ user, handleLogout }) => {
   return (
     
     <div className="container">
-      {user ? 
-        <div className="user-nav">
-          <ul>
-            <li><Link to="/profile">View Profile</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          </ul>
-        </div>
-      :
-        <div className="reg-nav">
-          <ul>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </div>
-      }
+      <div className="navigation">
+        {user ? 
+          <div className="user-nav">
+            <ul>
+              <li><Link to="/profile">View Profile</Link></li>
+              <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            </ul>
+          </div>
+        :
+          <div className="reg-nav">
+            <ul>
+            <li><Link to="/login">Log In</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+            </ul>
+          </div>
+        }
+      </div>
       <section className="one">
         <div className="title-info">
           <br/>
           <br/>
           <br/>
           <h1>Folio</h1>
+          <h2>An App for Freelancers</h2>
         </div>
       </section>
       <section className="spacer1"></section>
