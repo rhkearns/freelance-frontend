c08b89912a93722439f72ddad0a5ffc725d5f879
Cole-Fasano <cole.fasano@outlook.com>
2021-12-23 03:01:20 -0600

    fix sign up form

--- src/components/Forms/signUpForm.jsx
+++ src/components/Forms/signUpForm.jsx
@@ -95,7 +95,7 @@ const SignupForm = props => {
           onChange={handleChange}
         />
       </FormGroup>
-      <Button disabled={isFormInvalid()}></Button> 
+      <Button disabled={isFormInvalid()}>Sign Up</Button> 
     </Form>
    </div>
   )
