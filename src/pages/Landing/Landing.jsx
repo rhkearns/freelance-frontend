11fbe77d44ae680b111bd099a35e4d36a6e9b89f
Cole-Fasano <cole.fasano@outlook.com>
2022-01-06 12:19:46 -0600

    complete section 3 -small

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -20,7 +20,7 @@ const Landing = ({ user, handleLogout, signInFormStatus, setSignInFormStatus,  h
           <div className="reg-nav">
             <ul>
               <li><button className='sign-in-button' onClick={() => setSignInFormStatus(!signInFormStatus)}>Sign In</button></li>
-              <li><button className="sign-up-button" onClick={() => setSignUpFormStatus(!signUpFormStatus)}>Create Account</button></li>
+              <li><button className="sign-up-button" onClick={() => setSignUpFormStatus(!signUpFormStatus)}>Get Started</button></li>
             </ul>
           </div>
         }
@@ -55,9 +55,7 @@ const Landing = ({ user, handleLogout, signInFormStatus, setSignInFormStatus,  h
       </section>
       <section className="spacer2"></section>
       <section className="three">
-        <h1 className='CTA'>Start your Folio today!</h1>
-        <h2>Whether you have previous work looking for an organized place to live, or are looking to have a great tool in your work as a freelancer, you can do it all with Folio. </h2>
-        <h1>Get Started</h1>
+        <h2>Whether you have previous work looking for an organized place to live, or are looking to have a great tool for your work as a freelancer, you can do it all with Folio. </h2>
       </section>
     </div>
   )
