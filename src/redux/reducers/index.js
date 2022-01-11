7254128d90e82c4d132523f9d14041cef182c247
rhkearns <ryankearns217@gmail.com>
2022-01-11 11:25:49 -0500

    merge reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { projectReducer } from './projectReducer'
+import { clientReducer } from './clientReducer'
 
 const reducers = combineReducers({
   allProjects: projectReducer,
+  allClients: clientReducer,
 })
 
 export default reducers
